05254edb2b9ce6221c750219528364f838611f30
Luke <luke@Lukes-MacBook-Air.local>
2023-06-04 21:36:15 -0600

    Started working on Primitive Types

---
+++ Section 6/Primitive Types/src/main.cpp
@@ -0,0 +1,16 @@
+//Section 6
+//Primitive Types
+
+
+#include <iostream>
+
+int main(){
+
+	char middle_initial {'J'};
+	std::cout << "My middle initial is "  << middle_initial << std::endl;
+
+	unsigned short int age {20};
+	std::cout << "My age is " << age << std::endl;
+
+	return 0;
+}
